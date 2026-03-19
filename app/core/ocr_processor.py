7bcbbbbdf48874482bc1b0f649e1c7c142ec3590
Ibezim Uzoma Joseph <uzoma@Ibezims-MacBook-Air.local>
2026-03-19 13:44:06 +0100

    Committed ocr processor

--- app/core/ocr_processor.py
+++ app/core/ocr_processor.py
@@ -5,3 +5,20 @@ import pdf2image
 from typing import Union, List
 import logging
 
+class OCRProcessor:
+    def __init__(self, languages: List[str] = ['eng']):
+        self.languages = '+'.join(languages)
+        
+    def process_pdf(self, pdf_path: str) -> str:
+        """Extract text from PDF"""
+        images = pdf2image.convert_from_path(pdf_path)
+        text = ""
+        for image in images:
+            text += self.process_image(image)
+        return text
+    
+    def process_image(self, image: Union[str, Image.Image]) -> str:
+        """Extract text from image"""
+        if isinstance(image, str):
+            image = Image.open(image)
+        return pytesseract.image_to_string(image, lang=self.languages)
